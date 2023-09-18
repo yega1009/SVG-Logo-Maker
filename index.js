c086c9f6cd63e2e416c77196a30e43a51e4a324f
Yega Cho <yega1009@gmail.com>
2023-09-18 13:56:48 -0400

    created function that generates logo in index.js, created svg.js

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
 const inquirer = require('inquirer');
 const { Triangle, Circle, Square } = require('./lib/shapes');
+const { SVG } = require("./lib/svg");
+const { writeFile } = require("fs/promises");
 
 function getLogoDetails() {
     return inquirer.prompt([
         {
+            type: "input",
             name: "text",
-            type: "input",
             message: "Enter text for logo. (up to 3 characters)",
-            validate: (text) => text.length <= 3 || "The text must be less or equal to 3 characters",
+            validate: input => (input.length > 0 && input.length <= 3) ? true : 'Text should be up to 3 characters only.',
         },
         {
+            type: "input",
             name: "textColor",
-            type: "input",
-            message: "Enter color for text",
+            message: 'Enter the text color (color keyword or hexadecimal):'
         },
         {
-            name: "shape",
             type: "list",
-            message: "Choose a shape for the logo",
-            options: ["triangle", "circle", "square"],
-        }
+            name: "shape",
+            message: "Select a shape:",
+            choices: ["triangle", "circle", "square"],
+        },
         {
-            name: "shapeColor",
-            type: "input",
-            message: "Enter color for shape",
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter the shape color (color keyword or hexadecimal):'
         }
     ]);
 }
+
+function generateLogo() {
+    getLogoDetails()
+        .then(({ text, textColor, shape, shapeColor }) => {
+            let logoShape;
+            switch (shape) {
+                case "triangle":
+                    logoShape = new Triangle();
+                    break;
+                case "circle":
+                    logoShape = new Circle();
+                    break;
+                case "square":
+                    logoShape = new Square();
+                    break;
+            }
+
+            const svgGenerator = new SVG(text, textColor, logoShape);
+            return writeFile('logo.svg', svgGenerator.render());
+        })
+        .then(() => {
+            console.log("Generated logo");
+        })
+        .catch((error) => {
+            console.log(error);
+            console.log("Unable to generate logo");
+        });
+}
+
+generateLogo();
