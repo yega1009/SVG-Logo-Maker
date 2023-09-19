7545b0c9fdab004ffb26a4f61ee5585009405f7c
Yega Cho <yega1009@gmail.com>
2023-09-19 16:32:17 -0400

    added descriptive comments and readme file

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # SVG-Logo-Maker
+
+## Link to Deployed Application
+<a href="https://drive.google.com/file/d/1R3VYfjYHnMX5cfVnKgfJ5xizLEgJzy0j/view">Walk-through Video</a>
+
+## Description
+
+- This is a Node.js command-line application that allows users to generate a simple SVG logo based on their inputs. 
+- With this tool, users can select a shape (triangle, circle, or square), specify colors, and add texts to the logo.
+
+## Usage
+
+- Run `npm install` to install the necessary packages.
+- Start the application by running `node index.js`.
+- Follow the command-line prompts to create your SVG logo.
+- Once completed, the generated SVG fi- le named `logo.svg` will be written in the root directory.
+- Run `npm test` to run tests
+
+## Installation
+N/A
+  
+## Contribution
+N/A
+
+## Tests
+N/A
+
+## License 
+This project is covered under the [MIT](https://opensource.org/licenses/MIT) license.
+
+## Questions
+For any questions, please reach out to me at the following:
+
+- [GitHub Profile](https://github.com/yega1009)
+- Email: yega1009@gmail.com
+
--- index.js
+++ index.js
@@ -1,14 +1,24 @@
+// Import inquirer module for user prompts.
 const inquirer = require('inquirer');
+// Destructure and import the shape classes from the shapes module.
 const { Triangle, Circle, Square } = require('./lib/shapes');
+// Destructure and import the SVG class from the svg module.
 const { SVG } = require("./lib/svg");
+// Destructure and import the writeFile method from the fs/promises module for writing to files.
 const { writeFile } = require("fs/promises");
 
+// Function to prompt user for details about the logo they wish to generate.
 function getLogoDetails() {
+    // Use inquirer to prompt the user with a series of questions.
     return inquirer.prompt([
         {
+            // Input type for text.
             type: "input",
+            // Name to reference the answer later.
             name: "text",
+            // Message displayed to the user.
             message: "Enter text for logo. (up to 3 characters)",
+            // Validate that the entered text is up to 3 characters long.
             validate: input => (input.length > 0 && input.length <= 3) ? true : 'Text should be up to 3 characters only.',
         },
         {
@@ -30,10 +40,14 @@ function getLogoDetails() {
     ]);
 }
 
+// Function to generate an SVG logo based on user input.
 function generateLogo() {
+    // Call function to prompt the user for logo details.
     getLogoDetails()
         .then(({ text, textColor, shape, shapeColor }) => {
+            // Declare a variable to hold the selected shape instance.
             let logoShape;
+            // Based on the shape selected by the user, instantiate the corresponding shape class.
             switch (shape) {
                 case "triangle":
                     logoShape = new Triangle();
@@ -46,16 +60,21 @@ function generateLogo() {
                     break;
             }
 
+            // Create a new SVG generator instance with the text, text color, and chosen shape.
             const svgGenerator = new SVG(text, textColor, logoShape);
+            // Use the render method of the SVG instance and write the resulting SVG to 'logo.svg'.
             return writeFile('logo.svg', svgGenerator.render());
         })
+        // If the logo generation is successful, inform the user.
         .then(() => {
             console.log("Generated logo");
         })
+        // If there's an error, log it and inform the user of the failure.
         .catch((error) => {
             console.log(error);
             console.log("Unable to generate logo");
         });
 }
 
+// Start the logo generation process by calling the generateLogo function.
 generateLogo();
